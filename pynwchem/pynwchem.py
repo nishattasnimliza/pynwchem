8de5efcecd3882dcd75630992bd910c5d5b69ec4
rangsimanketkaew <rangsiman1993@gmail.com>
2019-11-05 16:45:39 +0700

    Create template files

---
+++ pynwchem/pynwchem.py
@@ -0,0 +1,9 @@
+"""
+PyNWChem : Py(without thon) + NWChem
+
+By Rangsiman Ketkaew <rangsiman1993@gmail.com>
+Official webiste: https://github.com/rangsimanketkaew/pynwchem
+Install: pip install pynwchem --upgrade --user
+Uninstall: pip uninstall pynwchem
+"""
+
